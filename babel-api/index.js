aca7a853c1b76af8255748398ba3816a11d12833
xinxin.yuan <xinxin.yuan@trip.com>
2022-10-13 01:11:00 +0800

    完善文档

--- babel-api/index.js
+++ babel-api/index.js
@@ -1,18 +1,10 @@
 const babel = require("@babel/core");
 const fs = require("fs");
-fs.readFile("./source_code.js", "utf8", function (err, datastr) {
+fs.readFile("./source_code.js", "utf8", async function (err, sourceCode) {
   console.log("读取成功后的" + err);
-  console.log("读取成功后的" + datastr);
-  babel.transform(
-    datastr,
-    {
-      plugins: ["@babel/plugin-transform-arrow-functions"],
-    },
-    function (err, result) {
-      console.log("code===>", result.code);
-      console.log("map===>", result.map);
-      console.log("ast===>", result.ast);
-      console.log("result===>", JSON.stringify(result));
-    }
-  );
+  console.log("读取成功后的" + sourceCode);
+  const targetCode = babel.transform(sourceCode, {
+    plugins: ["@babel/plugin-transform-arrow-functions"],
+  });
+  console.log(targetCode.code)
 });
